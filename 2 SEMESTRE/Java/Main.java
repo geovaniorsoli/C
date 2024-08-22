3de087a4b9844af4cec73102f5b29858fb81280d
geovaniorsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-08-22 19:43:15 -0300

    java

---
+++ 2 SEMESTRE/Java/Main.java
@@ -0,0 +1,53 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("vai toma no cu java 👆");
+        System.out.println("boa noite ");
+        System.out.println("boa tarde");
+        System.out.print("isso vai ficar na mesma linha");
+        System.out.print("teste do print acho q fica na mesma linha tem que testar");
+        System.out.println("1");
+        //Operações matematicas dentro do print
+        System.out.println(2);
+        System.out.println(1 + 5);
+
+        /* teste de comentario em grande escala
+         * e varias linhas
+         * 
+         */
+
+
+         //Variaveis
+        String cachorro = "toddy";
+        System.out.println(cachorro);
+
+        String comida = "Hamburguer";
+        System.out.println(comida);
+
+        String carro = "tikinho march nissan";
+        System.out.println(carro);
+
+        int numero = 34;
+        System.out.println(numero);
+
+        int qualquerNumero = 909;
+        System.out.println(qualquerNumero);
+
+        int variaveisVazias;
+        variaveisVazias = 300;
+        System.out.println(variaveisVazias);
+
+        float numeroFloat = 35.3f;
+        float outroNumeroFlutuante = 34.1f;
+        float nuvem = 34.1f;
+
+        char letra = 'a';
+        System.out.println(letra);
+
+        boolean verdade = true;
+        boolean mentira = false; 
+
+    }
+
+} 
+
---
+++ 2 SEMESTRE/Java/calcularCachorro.java
@@ -0,0 +1,17 @@
+public class calcularCachorro {
+    private double cachorro1;
+    private double cachorro2;
+
+    public calcularCachorro(double cachorro1, double cachorro2) {
+        this.cachorro1;
+        this.cachorro2;
+    }
+
+    public double calcularDoisCachorros(){
+        return (cachorro1 + cachorro2) / 2;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
